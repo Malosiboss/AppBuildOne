46b6658d977d48e882299a9b1ba142726927ce43
NoahNua <NoahNua@hawaii.edu>
2021-02-11 10:46:29 -1000

    Task for VC 2 Completed

--- AppBuildOne/seconedViewController.swift
+++ AppBuildOne/seconedViewController.swift
@@ -8,22 +8,33 @@
 import UIKit
 
 class seconedViewController: UIViewController {
+    var X = 21
+    let Y = 34
 
+    @IBOutlet weak var label1: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.label1.text = ""
+        
     }
     
+    @IBAction func pressedCalculateButton(_ sender: Any)
+    {
+let answer =  X + Y
+        
+self.label1.text = String(answer)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if answer == 55
+        {
+            self.view.backgroundColor = UIColor.green
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.orange
     }
-    */
+    
+   
 
+}
 }
